1cdb2423f9a026435c6ecc3c331ea3f3acd3dff0
Jordan Mavrogeorge <jordomav@gmail.com>
2017-06-23 18:43:47 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var alexa = require('alexa-app');
+var app = new alexa.app();
+
+
+/**
+ * Error handler for any thrown errors.
+ */
+app.error = function(exception, request, response) {
+    response.say('Sorry, something bad happened');
+};
+
+// Connect to lambda
+exports.handler = app.lambda();
